9c582cf6c44a4d180d78d0170df7d557833eaada
PrimalCat-Real <romanenko.denis2003@gmail.com>
2025-03-01 11:19:51 +0200

    fix method

--- bot/buttons/accept_form_button.py
+++ bot/buttons/accept_form_button.py
@@ -16,26 +16,27 @@ class AcceptFormButton(discord.ui.Button):
             async def on_submit(self, interaction: discord.Interaction):
                 reason = self.reason.value if self.reason.value else "Не указано"
                 await self.process_approval(interaction, reason)
+                
+            async def process_approval(self, interaction, reason):
+                        self.db_manager.forms.update_one({"mc_username": self.form_data["mc_username"]}, {"$set": {"status": "approved", "approved_by": interaction.user.id, "approved_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "reason": reason}})
+                        embed = interaction.message.embeds[0]
+                        embed.set_field_at(embed.fields.index(next(field for field in embed.fields if field.name == "Статус")), name="Статус", value=FORM_STATUSES["approved"].name)
+                        embed.add_field(name="Одобрено", value=f"<@{interaction.user.id}>", inline=False)
+                        embed.add_field(name="Время одобрения", value=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), inline=False)
+                        embed.add_field(name="Причина", value=reason, inline=False)
 
+                        embed.color = discord.Color(FORM_STATUSES["approved"].color)
+
+                        await interaction.response.edit_message(embed=embed, view=None)
+                        self.db_manager.discord_messages.delete_one({"message_id": interaction.message.id})
+                        # self.db_manager.forms.update_one({"mc_username": self.form_data["mc_username"]}, {"$set": {"status": "approved", "approved_by": interaction.user.id, "approved_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}})
+
+                        # embed = interaction.message.embeds[0]
+                        # embed.set_field_at(embed.fields.index(next(field for field in embed.fields if field.name == "Статус")), name="Статус", value=FORM_STATUSES["approved"].name)
+                        # embed.add_field(name="Одобрено", value=f"<@{interaction.user.id}>", inline=False)
+                        # embed.add_field(name="Время одобрения", value=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), inline=False)
+
+                # await interaction.response.edit_message(embed=embed, view=None)
         await interaction.response.send_modal(ReasonModal())
 
-    async def process_approval(self, interaction, reason):
-        self.db_manager.forms.update_one({"mc_username": self.form_data["mc_username"]}, {"$set": {"status": "approved", "approved_by": interaction.user.id, "approved_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "reason": reason}})
-        embed = interaction.message.embeds[0]
-        embed.set_field_at(embed.fields.index(next(field for field in embed.fields if field.name == "Статус")), name="Статус", value=FORM_STATUSES["approved"].name)
-        embed.add_field(name="Одобрено", value=f"<@{interaction.user.id}>", inline=False)
-        embed.add_field(name="Время одобрения", value=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), inline=False)
-        embed.add_field(name="Причина", value=reason, inline=False)
-
-        embed.color = discord.Color(FORM_STATUSES["approved"].color)
-
-        await interaction.response.edit_message(embed=embed, view=None)
-        self.db_manager.discord_messages.delete_one({"message_id": interaction.message.id})
-        # self.db_manager.forms.update_one({"mc_username": self.form_data["mc_username"]}, {"$set": {"status": "approved", "approved_by": interaction.user.id, "approved_at": datetime.now().strftime('%Y-%m-%d %H:%M:%S')}})
-
-        # embed = interaction.message.embeds[0]
-        # embed.set_field_at(embed.fields.index(next(field for field in embed.fields if field.name == "Статус")), name="Статус", value=FORM_STATUSES["approved"].name)
-        # embed.add_field(name="Одобрено", value=f"<@{interaction.user.id}>", inline=False)
-        # embed.add_field(name="Время одобрения", value=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), inline=False)
-
-        # await interaction.response.edit_message(embed=embed, view=None)
+      
